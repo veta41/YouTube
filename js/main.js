823fb499f73fa4664a4f829e8bbcb1ca65fdd9d4
Svitlana Chudesa <chsveta34@gmail.com>
2021-12-26 23:18:51 +0200

    фиксит меню и шапку, добавляет контент

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+ 
+  loop: true,
+  slidesPerView:6,
+
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+
+ 
+});
+
